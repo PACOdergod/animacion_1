24bbbbb18479db521eaf8a8a89f898caa90dc56c
paco <paco.33353@gmail.com>
2020-11-25 15:12:34 -0600

    El rectangulo rota cuando inicia la app, falta implementar el boton

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:animaciones_app/pages/animaciones_pages.dart';
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animaciones',
+      home: AnimacionesPage(),
+    );
+  }
+}
