de5d7e42cc0429be8a8b95740ab519187d5bbb1e
paco <paco.33353@gmail.com>
2020-11-25 18:18:33 -0600

    el cuadrado hace una vuelta y regresa

--- lib/pages/animaciones_pages.dart
+++ lib/pages/animaciones_pages.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as Math;
 
 class AnimacionesPage extends StatelessWidget {
   @override
@@ -26,7 +27,15 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
     controller =
         new AnimationController(vsync: this, duration: Duration(seconds: 4));
 
-    rotacion = Tween(begin: 0.0, end: 2.0).animate(controller);
+    rotacion = Tween(begin: 0.0, end: 2.0 * Math.pi).animate(controller);
+
+    controller.addListener(() {
+      //print(controller.status);
+
+      if (controller.status == AnimationStatus.completed) {
+        controller.reverse();
+      }
+    });
 
     super.initState();
   }
