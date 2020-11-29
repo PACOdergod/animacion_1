0d78c1b567d3d8a2355b91655dda4cf9c0f4ded3
paco <paco.33353@gmail.com>
2020-11-28 21:12:42 -0600

    El cuadro desaparece al final de la animacion

--- lib/pages/animaciones_pages.dart
+++ lib/pages/animaciones_pages.dart
@@ -22,6 +22,7 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
   AnimationController controller;
   Animation<double> rotacion;
   Animation<double> opacidad;
+  Animation<double> opacidadOut;
   Animation<double> moverDerecha;
   Animation<double> agrandar;
 
@@ -31,8 +32,10 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
         new AnimationController(vsync: this, duration: Duration(seconds: 4));
 
     opacidad = Tween(begin: 0.1, end: 1.0).animate(CurvedAnimation(
-        parent: controller,
-        curve: Interval(0.75, 1.0, curve: Curves.bounceIn)));
+        parent: controller, curve: Interval(0.0, 1.0, curve: Curves.easeOut)));
+
+    opacidadOut = Tween(begin: 0.0, end: 1.0).animate(CurvedAnimation(
+        parent: controller, curve: Interval(0.75, 1.0, curve: Curves.easeOut)));
 
     moverDerecha = Tween(begin: 0.0, end: 200.0).animate(CurvedAnimation(
       parent: controller,
@@ -79,7 +82,7 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
           child: Transform.rotate(
               angle: rotacion.value,
               child: Opacity(
-                opacity: opacidad.value,
+                opacity: opacidad.value - opacidadOut.value,
                 child: Transform.scale(scale: agrandar.value, child: child),
               )),
         );
